05cfcde8b4b5a02cfe52fff08fe61e1eed187e8c
Flamgop <firefaefly@gmail.com>
2023-12-05 22:22:24 -0800

    fixed thing

--- index.js
+++ index.js
@@ -83,6 +83,4 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(port);
